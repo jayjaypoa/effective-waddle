7d6863049e5bfc69cf23fc066bac28dc41d2fce8
unknown <08103879@w3043445.portoalegre.pucrsnet.br>
2017-06-06 21:57:24 -0300

    #2

---
+++ src/FormattedTextFieldDemoData.java
@@ -0,0 +1,40 @@
+
+public class FormattedTextFieldDemoData {
+	
+	public double amount;
+	public double rate;
+	public int numPeriods;
+	
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public double getRate() {
+		return rate;
+	}
+
+	public void setRate(double rate) {
+		this.rate = rate;
+	}
+
+	public int getNumPeriods() {
+		return numPeriods;
+	}
+
+	public void setNumPeriods(int numPeriods) {
+		this.numPeriods = numPeriods;
+	}
+
+	public FormattedTextFieldDemoData(double amount, double rate, int numPeriods) {
+		this.amount = amount;
+		this.rate = rate;
+		this.numPeriods = numPeriods;
+	}
+	
+	
+	
+}
